c8f9458ee3ec346bb8f0dfc970a78f87f27fb3eb
Nicho Reyes <nicho.reyes@deltalogic.ch>
2020-12-29 16:33:19 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+const trainerEvents = require('./events.json');
+const events = JSON.parse(JSON.stringify(trainerEvents).toUpperCase());
+
+const eventEffects = ['AFFINITY', 'STR', 'MOOD', 'STAMINA', 'DEX', 'INT', 'MNT', 'SP', 'SKILL DISCOUNT', 'GP'];
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if (msg.content.toLowerCase().startsWith("!trainer")) {
+        const trainerName = msg.content.replace('!trainer', '').toUpperCase().trim();
+        const trainerEvent = events[trainerName];
+        if (trainerEvent != null && trainerEvent != undefined) {
+            const arrEvents = getEvent(trainerEvent);
+            msg.channel.send(arrEvents.join(''));
+        }
+    }
+});
+
+
+function getEvent(trainerEvent) {
+    const eventArr = [];
+    Object.keys(trainerEvent).forEach(key => {
+        let eventEffect = `***${key}***: ` + "```";
+        Object.keys(trainerEvent[key]).forEach(eventKey => {
+            eventEffect += `${eventKey}: ${trainerEvent[key][eventKey]} \n`;
+        });
+        eventEffect += "```";
+        eventArr.push(eventEffect);
+    });
+    return eventArr;
+}
